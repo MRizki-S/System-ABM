b7169fc06f2e86dec79e96ea31a3b4094edf3bbe
Reizkyyy <rizkisus0@gmail.com>
2025-07-27 20:03:20 +0700

    update build vite

--- resources/views/Auth/login.blade.php
+++ resources/views/Auth/login.blade.php
@@ -6,7 +6,7 @@
     <meta content="width=device-width, initial-scale=1.0" name="viewport" />
     <title>Login Page</title>
     {{-- @vite('resources/css/app.css') --}}
-    <link rel="stylesheet" href="{{ asset('build/assets/app-Cx_wZTzM.css') }}">
+    <link rel="stylesheet" href="{{ asset('build/assets/app-C2THiBka.css') }}">
     <script src="{{ asset('build/assets/app-DNxiirP_.js') }}"></script>
 
 
